1903e1fcb12bb9444a3338f4fdb137f8394a74ef
Lulu AlKulaib <lkulaib@kisr.edu.kw>
2017-03-20 17:03:54 -0400

    NLC Test

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.ibm.watson.developer_cloud.natural_language_classifier.v1.NaturalLanguageClassifier;
 import com.ibm.watson.developer_cloud.natural_language_classifier.v1.model.Classification;
 import com.ibm.watson.developer_cloud.natural_language_classifier.v1.model.Classifier;
+import com.ibm.watson.developer_cloud.natural_language_classifier.v1.model.Classifiers;
 
 /**
  * Servlet implementation class SimpleServlet
@@ -26,13 +27,19 @@ public class SimpleServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         response.getWriter().print("Test!");
+        
         NaturalLanguageClassifier service = new NaturalLanguageClassifier();
-        service.setUsernameAndPassword("{f24bfe3a-e831-41b3-beec-9b11ba14c421}","{oBFD4XzAhwa2}");
+        service.setUsernameAndPassword("f24bfe3a-e831-41b3-beec-9b11ba14c421","oBFD4XzAhwa2");
+        
+        Classifiers classifiers = service.getClassifiers().execute();
+        response.getWriter().print(classifiers);
         
         Classifier classifier = service.getClassifier("90e7b7x198-nlc-3089").execute();
+        response.getWriter().print(classifier.getStatus());
+  
         System.out.println(classifier);
         
-        Classification classification = service.classify("10D41B-nlc-1",
+        Classification classification = service.classify("90e7b7x198-nlc-3089",
         		  "إن ثورة ٣٠ يونيو ليست ببعيدة، ولن تكون أبدا بعيدة.. وستظل حية نابضة بجهد وإصرار عشرات الملايين الذين فجروها").execute();
         		System.out.println(classification);
         
